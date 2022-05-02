286377b9f86cf790ee69743931b47ce46918533f
Your Name <you@example.com>
2022-05-02 10:22:58 +0800

    update

--- py/units/tools.py
+++ py/units/tools.py
@@ -3,7 +3,7 @@ import datetime
 import uuid
 
 # from jo_user.models.mongo_models import Follow, Fans, Friend, BadgeSource
-from models.mongo_models import Gift, Follow, User
+# from models.mongo_models import Gift, Follow, User
 from settings import log_conf
 from units.redis_tools import redis_conn
 
@@ -61,14 +61,14 @@ def detect_safe_search_uri(uri, client):
     return (safe.adult, safe.medical, safe.spoof, safe.violence)
 
 
-def get_gift_info(gid):
-    ret = dict()
-    gift_obj = Gift.objects(git=gid).first()
-    if gift_obj:
-        ret['gnama'] = gift_obj['gname']
-        ret['gicon'] = gift_obj['gicon']
-        ret['price'] = gift_obj['price']
-    return ret
+# def get_gift_info(gid):
+#     ret = dict()
+#     gift_obj = Gift.objects(git=gid).first()
+#     if gift_obj:
+#         ret['gnama'] = gift_obj['gname']
+#         ret['gicon'] = gift_obj['gicon']
+#         ret['price'] = gift_obj['price']
+#     return ret
 
 
 def check_age_from_birthday(birthday):
@@ -88,8 +88,8 @@ def check_age_from_birthday(birthday):
 #     producer.close()
 #     return True
 
-def check_is_followed(uid, aid):
-    return 1 if Follow.objects(uid=uid, aid=aid).first() else 0
+# def check_is_followed(uid, aid):
+#     return 1 if Follow.objects(uid=uid, aid=aid).first() else 0
 
 
 # def check_is_block(uid, aid):
@@ -125,20 +125,20 @@ def get_free_msg_key(uid):
     return "free_msg_key_{}".format(uid)
 
 
-def get_coins(uid):
-    user_obj = User.objects(id=uid).first()
-    return user_obj.coins
+# def get_coins(uid):
+#     user_obj = User.objects(id=uid).first()
+#     return user_obj.coins
 
 
-def check_user_status(uid):
-    status = 0
-    user_obj = User.objects(id=uid).first()
-    if user_obj:
-        if user_obj.hidden_online == 0:
-            status = user_obj.status
-        else:
-            status = 0
-    return status
+# def check_user_status(uid):
+#     status = 0
+#     user_obj = User.objects(id=uid).first()
+#     if user_obj:
+#         if user_obj.hidden_online == 0:
+#             status = user_obj.status
+#         else:
+#             status = 0
+#     return status
 
 
 def gen_agora_token(room_id, agora_id):
